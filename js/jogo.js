9d0c7b187624469bd81048a353ad9f7ec628d4bd
andrew <andrewoliveira282@gmail>
2022-03-04 14:56:49 -0300

    ajustando a tela do game para diversos aparelhos

---
+++ js/jogo.js
@@ -0,0 +1,10 @@
+//alert('teste')
+//agora temos que encontrar a altura e a largura da pagina para que os mosquitos aparecam de forma correta
+var largura=0,altura=0
+function ajustaTamanhoPalcoJogo(){
+ largura = window.innerWidth
+ altura = window.innerHeight
+  console.log(altura,largura)
+
+}
+ajustaTamanhoPalcoJogo()
