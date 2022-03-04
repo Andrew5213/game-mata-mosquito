78282b56de6c9676ae5d30937f96b6d29adf6d3a
andrew <andrewoliveira282@gmail>
2022-03-04 20:01:28 -0300

    ajustando a tela

--- js/jogo.js
+++ js/jogo.js
@@ -4,7 +4,51 @@ var largura=0,altura=0
 function ajustaTamanhoPalcoJogo(){
  largura = window.innerWidth
  altura = window.innerHeight
-  console.log(altura,largura)
+  console.log(largura, altura)
 
 }
 ajustaTamanhoPalcoJogo()
+// agora vamos comecar a gerar as posições aleatoriamente nos eixos x e y
+function posicaoRandomica(){
+  if (document.getElementById('mosquito')){
+    document.getElementById('mosquito').remove()
+  }
+var posicaoX = Math.floor(Math.random() *largura) - 90
+var posicaoY = Math.floor(Math.random() * altura) - 90
+  posicaoX = posicaoX < 0 ? 0 :posicaoX
+  posicaoY = posicaoY < 0 ? 0: posicaoY
+console.log(posicaoX,posicaoY)
+var mosquito = document.createElement('img')
+  mosquito.src='imagens/mosca.png'
+  mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+  mosquito.style.left=posicaoX+'px'
+  mosquito.style.top=posicaoY+'px'
+  mosquito.style.position='absolute'
+  mosquito.id='mosquito'
+document.body.appendChild(mosquito)
+  tamanhoAleatorio()
+}
+function tamanhoAleatorio(){
+  var classe =Math.floor(Math.random()*3)
+  //console.log(classe)
+  switch (classe){
+    case 0:
+      return 'mosquito1'
+    case 1:
+      return 'mosquito2'
+
+    case 2:
+      return 'mosquito3'
+
+  }
+
+}
+function ladoAleatorio(){
+  var classe =Math.floor(Math.random()*2)
+switch (classe){
+  case 0:
+    return 'ladoA'
+  case 1:
+    return 'ladoB'
+}
+}
